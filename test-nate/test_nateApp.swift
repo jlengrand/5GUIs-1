99806e9c0ff6b6c06667f351f90cd7fd2f9fef06
Julien Lengrand-Lambert <julien@lengrand.fr>
2024-07-03 13:28:55 +0200

    my stuff

---
+++ test-nate/test_nateApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_nateApp.swift
+//  test-nate
+//
+//  Created by julien Lengrand-Lambert on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_nateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
